9d70f2259fffd000caea0b2e26d9a9db44491bd3
rafaeltab <ikbenrafaelbieze2001@gmail.com>
2024-04-04 23:28:37 +0200

    Noice!

--- .config/nvim/lua/base.lua
+++ .config/nvim/lua/base.lua
@@ -180,6 +180,21 @@ function setup(plugins)
       'theprimeagen/harpoon'
     },
     {
+      "folke/noice.nvim",
+      event = "VeryLazy",
+      opts = {
+        -- add any options here
+      },
+      dependencies = {
+        -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+        "MunifTanjim/nui.nvim",
+        -- OPTIONAL:
+        --   `nvim-notify` is only needed, if you want to use the notification view.
+        --   If not available, we use `mini` as the fallback
+        "rcarriga/nvim-notify",
+      }
+    },
+    {
       -- Add indentation guides even on blank lines
       'lukas-reineke/indent-blankline.nvim',
       main = "ibl",
