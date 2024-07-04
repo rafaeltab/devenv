29142b5354a12d67f969989657e7ef0772825f37
rafaeltab <ikbenrafaelbieze2001@gmail.com>
2024-07-04 17:38:48 +0200

    Update

--- .config/nvim/lua/filetypes.lua
+++ .config/nvim/lua/filetypes.lua
@@ -1,12 +1,3 @@
--- " my filetype file
--- 	if exists("did_load_filetypes")
--- 	  finish
--- 	endif
--- 	augroup filetypedetect
--- 	  au! BufRead,BufNewFile *.mine		setfiletype mine
--- 	  au! BufRead,BufNewFile *.xyz		setfiletype drawing
--- 	augroup END
-
 vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
   pattern = { "*.arb" },
   callback = function(ev)
