6ebc8f9bf11c4d8396e90c5f5e8c87a327fd428d
Rafael Bieze <methylium_rafael.bieze@citizenm.com>
2024-09-09 14:47:35 +0200

    Improvements

--- .config/nvim/lua/keybinds.lua
+++ .config/nvim/lua/keybinds.lua
@@ -10,6 +10,12 @@ OnLoad:add(function()
     vim.api.nvim_set_keymap("", "j", "h", { noremap = true })
     vim.api.nvim_set_keymap("", "h", ";", { noremap = true })
 
+    vim.api.nvim_set_keymap("n", "<C-W>;", "<C-W>l", { noremap = true, desc = "Go to the right window" })
+    vim.api.nvim_set_keymap("n", "<C-W>l", "<C-W>k", { noremap = true, desc = "Go to the bottom window" })
+    vim.api.nvim_set_keymap("n", "<C-W>k", "<C-W>j", { noremap = true, desc = "Go to the top window" })
+    vim.api.nvim_set_keymap("n", "<C-W>j", "<C-W>h", { noremap = true, desc = "Go to the left window" })
+    vim.api.nvim_set_keymap("n", "<C-W>h", "<Nop>", { noremap = true, silent = true })
+
     vim.keymap.set({ 'n' }, 'd', '"_d')
     vim.keymap.set({ 'n', 'v' }, 'x', '"_x')
     vim.keymap.set({ 'n', 'v' }, 'c', '"_c')
--- .config/nvim/lua/plugins/testing.lua
+++ .config/nvim/lua/plugins/testing.lua
@@ -43,67 +43,67 @@ end)
 
 
 OnLoad:add(function()
-	local notify = require('notify')
-	-- Function to get the test file path for the current buffer
-	local function get_test_file_path()
-		local buf_name = vim.api.nvim_buf_get_name(0) -- Get the current buffer name
-		local file_type = vim.bo.filetype         -- Get the current file type
+    local notify = require('notify')
+    -- Function to get the test file path for the current buffer
+    local function get_test_file_path()
+        local buf_name = vim.api.nvim_buf_get_name(0) -- Get the current buffer name
+        local file_type = vim.bo.filetype             -- Get the current file type
 
-		-- Function to handle Dart test file path
-		local function handle_dart()
-			-- Check if the current file is already a test file
-			if buf_name:find("test/") and buf_name:find("_test.dart$") then
-				notify("Current file is already a test file", "warn")
-				return nil
-			end
+        -- Function to handle Dart test file path
+        local function handle_dart()
+            -- Check if the current file is already a test file
+            if buf_name:find("test/") and buf_name:find("_test.dart$") then
+                notify("Current file is already a test file", "warn")
+                return nil
+            end
 
-			local test_path = buf_name:gsub("lib/", "test/"):gsub(".dart$", "_test.dart")
-			return test_path
-		end
+            local test_path = buf_name:gsub("lib/", "test/"):gsub(".dart$", "_test.dart")
+            return test_path
+        end
 
-		-- Switch case for different languages
-		if file_type == "dart" then
-			return handle_dart()
-		else
-			notify("Language not supported", "error")
-			return nil
-		end
-	end
+        -- Switch case for different languages
+        if file_type == "dart" then
+            return handle_dart()
+        else
+            notify("Language not supported", "error")
+            return nil
+        end
+    end
 
-	-- Function to open or create the test file
-	local function open_or_create_test_file()
-		local test_file_path = get_test_file_path()
-		if not test_file_path then
-			return
-		end
+    -- Function to open or create the test file
+    local function open_or_create_test_file()
+        local test_file_path = get_test_file_path()
+        if not test_file_path then
+            return
+        end
 
-		-- Get the directory of the test file path
-		local test_file_dir = test_file_path:match("(.*/)")
+        -- Get the directory of the test file path
+        local test_file_dir = test_file_path:match("(.*/)")
 
-		-- Check if the directory exists, if not, create it
-		if vim.fn.isdirectory(test_file_dir) == 0 then
-			local success = vim.fn.mkdir(test_file_dir, "p")
-			if success ~= 1 then
-				notify("Failed to create directory: " .. test_file_dir, "error")
-				return
-			end
-		end
+        -- Check if the directory exists, if not, create it
+        if vim.fn.isdirectory(test_file_dir) == 0 then
+            local success = vim.fn.mkdir(test_file_dir, "p")
+            if success ~= 1 then
+                notify("Failed to create directory: " .. test_file_dir, "error")
+                return
+            end
+        end
 
-		-- Check if the test file is already open in another buffer
-		for _, buf in ipairs(vim.fn.getbufinfo({ bufloaded = 1 })) do
-			if vim.fn.bufname(buf.bufnr) == test_file_path then
-				vim.api.nvim_set_current_buf(buf.bufnr)
-				notify("Switched to existing buffer for test file", "info")
-				return
-			end
-		end
+        -- Check if the test file is already open in another buffer
+        for _, buf in ipairs(vim.fn.getbufinfo({ bufloaded = 1 })) do
+            if vim.fn.bufname(buf.bufnr) == test_file_path then
+                vim.api.nvim_set_current_buf(buf.bufnr)
+                notify("Switched to existing buffer for test file", "info")
+                return
+            end
+        end
 
-		-- Open the file (create if it doesn't exist)
-		vim.api.nvim_command('edit ' .. test_file_path)
-		notify("Opened test file: " .. test_file_path, "info")
-	end
+        -- Open the file (create if it doesn't exist)
+        vim.api.nvim_command('edit ' .. test_file_path)
+        notify("Opened test file: " .. test_file_path, "info")
+    end
 
-	vim.keymap.set({ 'n' }, '<leader>ct', function()
-		open_or_create_test_file()
-	end, { desc = "[C]reate [T]est" })
+    vim.keymap.set({ 'n' }, '<leader>ct', function()
+        open_or_create_test_file()
+    end, { desc = "[C]reate [T]est" })
 end)
