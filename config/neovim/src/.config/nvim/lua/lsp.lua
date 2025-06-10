e3bad12af986e9686184fcd108d3c0e4571a7cd7
Rafael Bieze <methylium_rafael.bieze@citizenm.com>
2025-06-10 10:35:21 +0200

    Use conform for formatting instead of ale

--- config/neovim/src/.config/nvim/lua/lsp.lua
+++ config/neovim/src/.config/nvim/lua/lsp.lua
@@ -2,161 +2,170 @@ require "utils.on_attach"
 require "utils.plugins"
 
 Plugins:add({
-    {
-        "aznhe21/actions-preview.nvim",
-        dependencies = {
-            'telescope.nvim'
-        },
-        opts = function()
-            return {
-                telescope = vim.tbl_extend(
-                    "force",
-                    -- telescope theme: https://github.com/nvim-telescope/telescope.nvim#themes
-                    require("telescope.themes").get_dropdown(),
-                    -- a table for customizing content
-                    {
-                        layout_config = {
-                            width = 0.5,
-                        },
-                    }
-                ),
-            }
-        end
+  {
+    "aznhe21/actions-preview.nvim",
+    dependencies = {
+      'telescope.nvim'
     },
-    {
-        -- LSP Configuration & Plugins
-        'neovim/nvim-lspconfig',
-        dependencies = {
-            -- Automatically install LSPs to stdpath for neovim
-            'williamboman/mason.nvim',
-            'williamboman/mason-lspconfig.nvim',
-
-            -- Useful status updates for LSP
-            -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-            {
-                'j-hui/fidget.nvim',
-                opts = {},
-                tag = "legacy",
+    opts = function()
+      return {
+        telescope = vim.tbl_extend(
+          "force",
+          -- telescope theme: https://github.com/nvim-telescope/telescope.nvim#themes
+          require("telescope.themes").get_dropdown(),
+          -- a table for customizing content
+          {
+            layout_config = {
+              width = 0.5,
             },
+          }
+        ),
+      }
+    end
+  },
+  {
+    -- LSP Configuration & Plugins
+    'neovim/nvim-lspconfig',
+    dependencies = {
+      -- Automatically install LSPs to stdpath for neovim
+      'williamboman/mason.nvim',
+      'williamboman/mason-lspconfig.nvim',
 
-            -- Additional lua configuration, makes nvim stuff amazing!
-            'folke/neodev.nvim',
-        },
+      -- Useful status updates for LSP
+      -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
+      {
+        'j-hui/fidget.nvim',
+        opts = {},
+        tag = "legacy",
+      },
+
+      -- Additional lua configuration, makes nvim stuff amazing!
+      'folke/neodev.nvim',
     },
-    { 'folke/neodev.nvim', opts = {} },
-    {
-        'dense-analysis/ale',
-        config = function()
-            -- Configuration goes here.
-            local g = vim.g
+  },
+  { 'folke/neodev.nvim', opts = {} },
+  {
+    'dense-analysis/ale',
+    config = function()
+      -- Configuration goes here.
+      local g = vim.g
 
-            g.ale_linters_explicit = 1
+      g.ale_linters_explicit = 1
 
-            g.ale_fixers = {
-                javascript = { 'prettier' },
-                typescript = { 'prettier' },
-            }
-
-            g.ale_linters = {
-                javascript = { 'cspell' },
-                typescript = { 'cspell' },
-                lua = {},
-                markdown = { 'cspell' }
-            }
-        end
-    }
+      g.ale_linters = {
+        javascript = { 'cspell' },
+        typescript = { 'cspell' },
+        lua = {},
+        markdown = { 'cspell' }
+      }
+    end
+  },
+  {
+    'stevearc/conform.nvim',
+    config = function()
+      require("conform").setup({
+        formatters_by_ft = {
+          javascript = { "prettierd" },
+          typescript = { "prettierd" },
+        },
+        default_format_opts = {
+          lsp_format = "last"
+        }
+      })
+    end
+  }
 })
 
 
 LanguagesV2:configure_language(function()
-    --- @type LanguageConfig
-    return {
-        lspconfig = {
-            ['svelte'] = {},
-            ['html'] = {},
-            ['lua_ls'] = {
-                settings = {
-                    Lua = {
-                        workspace = { checkThirdParty = false },
-                        telemetry = { enable = false },
-                    },
-                }
-            },
-            ['buf_ls'] = {},
-            ['vale_ls'] = {
-                filetypes = { "markdown", "text" --[[ , "dart" ]] },
-            },
-        },
-        mason = { 'svelte', 'html', 'lua_ls', 'buf_ls', 'vale_ls' },
-        treesitter = {}
-    }
+  --- @type LanguageConfig
+  return {
+    lspconfig = {
+      ['svelte'] = {},
+      ['html'] = {},
+      ['lua_ls'] = {
+        settings = {
+          Lua = {
+            workspace = { checkThirdParty = false },
+            telemetry = { enable = false },
+          },
+        }
+      },
+      ['buf_ls'] = {},
+      ['vale_ls'] = {
+        filetypes = { "markdown", "text" --[[ , "dart" ]] },
+      },
+    },
+    mason = { 'svelte', 'html', 'lua_ls', 'buf_ls', 'vale_ls' },
+    treesitter = {}
+  }
 end)
 
 OnLoad:add(function()
-    -- Diagnostic keymaps
-    vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic message" })
-    vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = "Go to next diagnostic message" })
-    vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = "Open floating diagnostic message" })
-    vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = "Open diagnostics list" })
+  -- Diagnostic keymaps
+  vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic message" })
+  vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = "Go to next diagnostic message" })
+  vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = "Open floating diagnostic message" })
+  vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = "Open diagnostics list" })
 
-    vim.diagnostic.config({
-        underline = true
-    })
+  vim.diagnostic.config({
+    underline = true
+  })
 
-    -- nvim-cmp supports additional completion capabilities, so broadcast that to servers
-    local capabilities = vim.lsp.protocol.make_client_capabilities()
-    capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+  -- nvim-cmp supports additional completion capabilities, so broadcast that to servers
+  local capabilities = vim.lsp.protocol.make_client_capabilities()
+  capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
-    -- Setup mason so it can manage external tooling
-    require('mason').setup({
-        PATH = "prepend"
-    })
+  -- Setup mason so it can manage external tooling
+  require('mason').setup({
+    PATH = "prepend"
+  })
 
-    -- local lsp_util = require 'lspconfig.util'
-    -- vim.lsp.start({
-    --     cmd = { 'terramate-ls' },
-    --     filetypes = { 'terramate' },
-    --     name = 'terramate-ls',
-    --     root_dir = lsp_util.root_pattern('terramate.tm.hcl', '.git'),
-    -- })
+  -- local lsp_util = require 'lspconfig.util'
+  -- vim.lsp.start({
+  --     cmd = { 'terramate-ls' },
+  --     filetypes = { 'terramate' },
+  --     name = 'terramate-ls',
+  --     root_dir = lsp_util.root_pattern('terramate.tm.hcl', '.git'),
+  -- })
 
-    -- Ensure the servers above are installed
-    local mason_lspconfig = require 'mason-lspconfig'
+  -- Ensure the servers above are installed
+  local mason_lspconfig = require 'mason-lspconfig'
 
-    local language_config = LanguagesV2:build()
+  local language_config = LanguagesV2:build()
 
-    mason_lspconfig.setup {
-        ensure_installed = language_config.mason,
+  mason_lspconfig.setup {
+    ensure_installed = language_config.mason,
+  }
+
+  local function setup_server(server_name)
+    local server_config = language_config.lspconfig[server_name]
+    if not server_config then
+      return
+    end
+
+    local default_config = {
+      capabilities = capabilities,
+      on_attach = function(client, bufnr)
+        OnAttach:attach(client, bufnr)
+      end,
+      settings = {},
     }
 
-    local function setup_server(server_name)
-        local server_config = language_config.lspconfig[server_name]
-        if not server_config then
-            return
-        end
+    local config = vim.tbl_deep_extend("force", default_config, server_config)
 
-        local default_config = {
-            capabilities = capabilities,
-            on_attach = function(client, bufnr)
-                OnAttach:attach(client, bufnr)
-            end,
-            settings = {},
-        }
+    require("lspconfig")[server_name].setup(config)
+  end
 
-        local config = vim.tbl_deep_extend("force", default_config, server_config)
-
-        require("lspconfig")[server_name].setup(config)
-    end
-
-    for k in pairs(language_config.lspconfig) do
-        setup_server(k)
-    end
+  for k in pairs(language_config.lspconfig) do
+    setup_server(k)
+  end
 end)
 
 OnAttach:add(function(_, bufnr)
-    Nmap(bufnr, '<leader>ca', require("actions-preview").code_actions, '[C]ode [A]ction')
+  Nmap(bufnr, '<leader>ca', require("actions-preview").code_actions, '[C]ode [A]ction')
 
-    Nmap(bufnr, 'K', vim.lsp.buf.hover, 'Hover')
-    Nmap(bufnr, '<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-    Nmap(bufnr, 'gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+  Nmap(bufnr, 'K', vim.lsp.buf.hover, 'Hover')
+  Nmap(bufnr, '<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+  Nmap(bufnr, 'gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
 end)
--- config/neovim/src/.config/nvim/lua/plugins/formatting.lua
+++ config/neovim/src/.config/nvim/lua/plugins/formatting.lua
@@ -1,13 +1,18 @@
 local format = function()
-    vim.cmd("ALEFix")
-    vim.lsp.buf.format({ async = true })
+  -- vim.cmd("ALEFix")
+  -- vim.lsp.buf.format({
+  --   async = true,
+  -- })
+  require("conform").format({
+    async = true
+  })
 end
 
 OnLoad:add(function()
-    vim.keymap.set('n', '<leader>f', format, { desc = "[F]ormat" })
+  vim.keymap.set('n', '<leader>f', format, { desc = "[F]ormat" })
 end)
 
 OnAttach:add(function(_, bufnr)
-    -- Create a command `:Format` local to the LSP buffer
-    vim.api.nvim_buf_create_user_command(bufnr, 'Format', format, { desc = 'Format current buffer with LSP' })
+  -- Create a command `:Format` local to the LSP buffer
+  vim.api.nvim_buf_create_user_command(bufnr, 'Format', format, { desc = 'Format current buffer with LSP' })
 end)
